06144a50970c8b3d8cdd7bcfa7b6b6c4baa14fa1
miikoa <miikoa@gmail.com>
2023-07-22 14:43:28 +0200

    added new template code

--- oeuvre.php
+++ oeuvre.php
@@ -15,6 +15,55 @@
     <?php include('oeuvres.php'); ?>
 
     <main>
+        <div class="oeuvre">
+            <article id="detail-oeuvre">
+                <div id="img-oeuvre">
+                    <?php
+                    if (isset($_GET['id'])) {
+                        $id = $_GET['id'];
+                        $foundPainting = null;
+                        foreach ($paintings as $painting) {
+                            if ($painting['id'] == $id) {
+                                $foundPainting = $painting;
+                                break;
+                            }
+                        }
+                        if ($foundPainting) {
+                            echo '<img src="' . $foundPainting['image'] . '" alt="' . $foundPainting['alt'] . '">';
+                        }
+                    }
+                    ?>
+                </div>
+                <div id="contenu-oeuvre">
+                    <?php
+                    if ($foundPainting) {
+                        echo '<h1>' . $foundPainting['title'] . '</h1>';
+                        echo '<p class="description">' . $foundPainting['paragraph-description'] . '</p>';
+                        echo '<p class="description-complete">' . $foundPainting['paragraph-complete'] . '</p>';
+                    } else {
+                        echo 'Artwork not found.';
+                    }
+                    ?>
+                </div>
+            </article>
+        </div>
+
+
+        <!--in the above code, I am checking to see if the id exists and if the $id
+            is a number.  after, to be safe, I am making the id a number and storing this information in the superglobal.--!>
+     
+             
+            
+   
+               
+            <!-- <article class="oeuvre">
+                <a href="oeuvre.php?id=1">
+                    <img src="img/clark-van-der-beken.png" alt="Dodomu">
+                    <h2>Dodomu</h2>
+                    <p class="description">Mia Tozerski</p>
+                </a>
+            </article> -->
+
         <!-- WRITE HERE THE PHP CODE TO GENERATE DYNAMICS OEUVRE PAGES -->
         <!-- USE GET PHP TO IDENTIFY THE ID IN THE URL -->
     </main>
@@ -23,3 +72,26 @@
 </body>
 
 </html>
+<!-- This is the layout copied from the oeuvre source code
+<main>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="img/pawel-czerwinski-3.png" alt="Aashaaheen Baadal">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1>Aashaaheen Baadal</h1>
+            <p class="description">Anaisha Devi</p>
+            <p class="description-complete">
+                Sur cette oeuvre conceptuelle à la fois organique, minérale et liquide, Anaisha Devi nous transporte dans un nuage noir envoûtant. Un sombre tableau qui, par son verni éclatant, rayonne tel un marbre poli. Une oeuvre à la cohérence transcendantale, exécutée à la perfection
+            </p>
+        </div>
+    </article>
+</main> -->
+
+<!-- This is the layout copied from the main page source code
+<article class="oeuvre">
+                <a href="oeuvre-1.html">
+                    <img src="img/clark-van-der-beken.png" alt="Dodomu">
+                    <h2>Dodomu</h2>
+                    <p class="description">Mia Tozerski</p>
+                </a>
--- oeuvres.php
+++ oeuvres.php
@@ -122,3 +122,20 @@ $paintings = [
     ]
 ];
 ?>
+
+
+<main>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="img/steve-johnson-3.png" alt="Blaue Gelbe Muster">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1>Blaue Gelbe Muster</h1>
+            <p class="description">Adelheid Von Schreiber</p>
+            <p class="description-complete">
+                Curabitur dui odio, porta vel tempor sed, consectetur vitae mi. Interdum et malesuada fames ac ante
+                ipsum primis in faucibus. Orci varius natoque penatibus nec.
+            </p>
+        </div>
+    </article>
+</main>
